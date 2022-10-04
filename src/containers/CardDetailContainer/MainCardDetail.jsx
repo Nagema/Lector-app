8be9663e48a91768e808f8c8b03aa105aa8969c7
angelica <angelica.cavero@gmail.com>
2022-10-04 14:59:09 +0200

    fixing styles

--- src/containers/CardDetailContainer/MainCardDetail.jsx
+++ src/containers/CardDetailContainer/MainCardDetail.jsx
@@ -2,19 +2,19 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { CardDetail } from "../../components/CardDetail/CardDetail";
 import { News } from "../../context/NewsContext";
+import "./styles.css";
 
 export const MainCardDetail = () => {
   const { id } = useParams();
   const { news } = useContext(News);
   return (
     <div>
-      {" "}
       {news
         .filter(
           (newItem) => newItem.title.replaceAll(" ", "-").toLowerCase() === id
         )
         .map((newItem) => (
-          <div key={newItem.link}>
+          <div className="main_detail_container" key={newItem.link}>
             <CardDetail newItem={newItem} />
           </div>
         ))}
