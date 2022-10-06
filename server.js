ba90045298c51f6dbd32c5496b11b1df7519bf61
angelica <angelica.cavero@gmail.com>
2022-10-06 22:47:27 +0200

    use lodash kebabCase for article id

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
-const axios = require('axios');
-const xml2js = require('xml2js');
+const express = require("express");
+const kebabCase = require("lodash/kebabCase");
+const app = express();
+const port = process.env.PORT || 3001;
+const axios = require("axios");
+const xml2js = require("xml2js");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/news', async (req, res) => {
-  const response = await axios.get('https://www.meneame.net/rss')
+app.get("/news", async (req, res) => {
+  const response = await axios.get("https://www.meneame.net/rss");
   const xmlText = response.data;
   // transform xml to json
   const feed = await new Promise((resolve, reject) => {
@@ -19,10 +20,10 @@ app.get('/news', async (req, res) => {
   const [channel] = feed.rss.channel;
   // map rss json to our own structure
   const news = channel.item.map((item) => ({
-    id: item.title[0]?.replace(/ /g, '-').toLowerCase(),
+    id: kebabCase(item.title),
     title: item.title[0],
     link: item.link[0],
-    pubDate: item.pubDate[0]?.split(' ').slice(1, 5).join(' '), // pubDate is in format ¨Tue, 04 Oct 2022 20:35:03 +0000¨, we want to only show the date and hour
+    pubDate: item.pubDate[0]?.split(" ").slice(1, 5).join(" "), // pubDate is in format ¨Tue, 04 Oct 2022 20:35:03 +0000¨, we want to only show the date and hour
     description: item.description[0],
     image: item["media:thumbnail"]?.[0].$.url,
     category: item["meneame:sub"]?.[0],
@@ -32,7 +33,7 @@ app.get('/news', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`REST server listening on port ${port}`)
-})
+  console.log(`REST server listening on port ${port}`);
+});
 
-app.use(express.static('build'));
+app.use(express.static("build"));
